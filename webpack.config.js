60c3a0da3a2c1358d70f03bf2a0d5fdcbf80551f
Ilya Muravev <ilya.yarn@gmail.com>
2016-09-10 13:27:48 +0400

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
+var webpack = require('webpack');
 
 module.exports = {
     entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
         './src/index.js'
     ],
     module: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel'
+            loader: 'react-hot-loader/webpack!babel'
         }]
     },
     resolve: {
@@ -19,6 +22,10 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './www'
-    }
+        contentBase: './www',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
